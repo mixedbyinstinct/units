4f3e3232ad3488335cfa8878bb9f69684d846b9b
mixedbyinstinct <92763310+mixedbyinstinct@users.noreply.github.com>
2022-09-10 04:31:42 -0400

    frontend

--- src/app.jsx
+++ src/app.jsx
@@ -5,22 +5,43 @@ import ReactDOM from 'react-dom';
 
 const GlobalStyles = createGlobalStyle`
 body {
+    background-color: #d0faf9;
     & h1 {
         font-family: system-ui;
         font-size: 32px;
+        color: #151919;
     }
     & p {
         font-family: system-ui;
         font-size; 18px;
+        color: #151919;
     }
     & input[type="text"] {
+        background-color: # d9fbfa;
         font-family: system-ui;
         font-size: 18px;
         height: 36px;
         text-align: center;
         border-radius: 4px;
+        color: #151919;
     }
 }
+`;
+
+const Display = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-flow: column nowrap;
+    beight: 200px;
+    width: 40vw;
+    background-color: #bdf6d9;
+    & h1 {
+        font-family: system-ui;
+        font-size: 40px;
+        color: #3e5a65;
+
+    }
 `;
 
 const Container = styled.div`
@@ -32,8 +53,8 @@ flex-grow: 1;
 `;
 
 const Button = styled.button`
-background-color: LightSeaGreen;
-color: #EEECED;
+background-color: #bae6fc;
+color: #151919;
 font-family: system-ui;
 height: 28px;
 font-size: 14px;
@@ -41,6 +62,7 @@ border-radius: 6px;
 `;
 
 const App = () => {
+    const [script, setScript] = useStare('');
     const [feet, setFeet] = useState('');
     const [meters, setMeters] = useState('');
     const [print, setPrint] = useState('');
@@ -48,9 +70,13 @@ const App = () => {
     function getFeet(e) {
         setFeet(e.target.value);
     }
+    function getScript(e) {
+        setScript(e.target.value);
+    }
     function convert() {
         let send = {
             number: feet,
+            script: script,
         }
         axios.post("/convert", send).then(({data}) => {
             setPrint(data.message);
@@ -64,9 +90,13 @@ const App = () => {
             <h1>Convert meters to feet</h1>
             <label>meters</label>
             <input type="text" onChange={getFeet} value={feet}/>
+            <br /><label>script</label>
+            <input type="text" onChange={getSscript} value={script}/>
             <Button onClick={convert}>Convert</Button>
-            <p>{print}</p>
+            <Display>
+            <h1>{print}</h1>
             <p>{meters}</p>
+            </Display>
         </Container>
     )
 }

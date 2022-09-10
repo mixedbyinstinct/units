cd72e10f4d158f0df067e1d9c8899a2ef90b98b7
mixedbyinstinct <92763310+mixedbyinstinct@users.noreply.github.com>
2022-09-10 17:37:00 -0400

    energizer bunny

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.post("/convert", async (req, res) => {
             console.log(err);
         }
         let dbo = db.db('personal-site-db');
-        const scriptPath = await dbo.collection("scripts").findOne({title: {$regex: /[script+]/}});
+        const scriptPath = await dbo.collection("scripts").findOne({title: /[script+]\.(py)+/ });
         console.log(scriptPath);
         const f2m = /^(\/.*\/.*\/.*\/.*\/((convert)+(ftm)+)\.*)/.test(scriptPath.path);
         if(f2m) {

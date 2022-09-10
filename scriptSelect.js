43471d02af1b8c123dbb6bbf1a1f31e32a6937c6
mixedbyinstinct <92763310+mixedbyinstinct@users.noreply.github.com>
2022-09-10 05:10:26 -0400

    no async?

--- scriptSelect.js
+++ scriptSelect.js
@@ -1,14 +1,14 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27018/personal-site-db';
 
-async function scriptSelector(script) {
+function scriptSelector(script) {
     let outScript;
-    await MongoClient.connect(url,  async function(err, db) {
+    MongoClient.connect(url,  function(err, db) {
         if(err) {
             console.log(err);
         }
         let dbo = db.db('personal-site-db');
-    const scriptPath =  await dbo.collection("scripts").findOne({title: {$regex: /[script*]/}});
+    const scriptPath =  dbo.collection("scripts").findOne({title: {$regex: /[script*]/}});
     console.log('found:' + scriptPath.path);
     outScript = scriptPath.path;
     })

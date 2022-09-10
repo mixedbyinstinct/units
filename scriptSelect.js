2d53e8665562e8f4921ed42318796b01df1aeb73
mixedbyinstinct <92763310+mixedbyinstinct@users.noreply.github.com>
2022-09-10 03:48:04 -0400

    typo

--- scriptSelect.js
+++ scriptSelect.js
@@ -1,4 +1,4 @@
-const MongoClient = requirqe('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 const url = 'mognodb://localhost:27018/personal-site-db';
 
 function scriptSelector(script) {
